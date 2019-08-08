1a6e4528dd7a74fa3327419dd8025a91c4e710bd
sakhawatcmd <sakhawat@constantmd.com>
2019-08-08 15:29:01 +0600

    Initial Commit

---
+++ MultipartImageUp/Controllers/FileUploadController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultipartImageUp.Models;
+
+namespace MultipartImageUp.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        //public IActionResult Index()
+        //{
+
+        //}
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(FileUpload model)
+        {
+            //var ab = model.File.Length;
+            var filePath = "D:\\";
+            
+            foreach (var fil in model.File)
+            {
+                var fullFilePath = Path.Combine(filePath, fil.FileName);
+                var stream = new FileStream(fullFilePath, FileMode.Create);
+            }
+            return View();
+        }
+    }
+}
---
+++ MultipartImageUp/Models/FileUpload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MultipartImageUp.Models
+{
+    public class FileUpload
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<IFormFile> File { get; set; }
+    }
+}
